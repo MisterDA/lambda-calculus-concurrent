2ab93a9b8148ae0c1029ca089fd6729fa44d5ca6
Antonin Décimo <antonin.decimo@gmail.com>
2018-05-09 13:58:49 +0200

    Add toplevel

--- src/run.ml
+++ src/run.ml
@@ -5,17 +5,33 @@ module RRI = Interpreter (Sched.RoundRobin)
 module RI = Interpreter (Sched.Random)
 
 let main () =
+  let n = Array.length (Sys.argv) in
+  if n < 2 || n > 3 then
+    failwith "Usage: ./run.native <sched> [file]";
   let sched = Sys.argv.(1) in
-  let filename = Sys.argv.(2) in
-  let input = open_in filename in
+  let toplevel = n = 2 in
+  let input =
+    if toplevel then begin
+      print_endline "Ctrl-D to execute.";
+      stdin
+      end
+    else
+      let filename = Sys.argv.(2) in
+      open_in filename
+  in
+
   let filebuf = Lexing.from_channel input in
   Random.self_init ();
 
   try
-    let p = Parser.prog Lexer.read filebuf in
-    if sched = "RRI" then RRI.eval' p
-    else if sched = "RI" then RI.eval' p
-    else failwith "Available interpreters: RRI (RoundRobinInterpreter), RI (RandomInterpreter)"
+    let rec aux () =
+      if toplevel then begin print_string "> "; flush stdout end;
+      let p = Parser.prog Lexer.read filebuf in
+      if sched = "RRI" then RRI.eval' p
+      else if sched = "RI" then RI.eval' p
+      else failwith "Available interpreters: RRI (RoundRobinInterpreter), RI (RandomInterpreter)";
+      if toplevel then aux ()
+    in aux ()
   with
   | Parser.Error ->
      Printf.eprintf "At offset %d: syntax error.\n%!"
